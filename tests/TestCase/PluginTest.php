cf522318a4202998a23d0953884db3f844e15dd0
dante di domenico <dante@didoda.com>
2023-07-06 14:04:59 +0200

    test: Plugin class

---
+++ tests/TestCase/PluginTest.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+namespace BEdita\Chatlas\Test\TestCase;
+
+use BEdita\Chatlas\Plugin;
+use Cake\Core\Configure;
+use Cake\Core\PluginApplicationInterface;
+use Cake\TestSuite\TestCase;
+
+/**
+ * @coversDefaultClass \BEdita\Chatlas\Plugin
+ */
+class PluginTest extends TestCase
+{
+    protected $plugin;
+
+    /**
+     * @inheritDoc
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->plugin = new class extends Plugin {
+            public function bootstrap(PluginApplicationInterface $app): void
+            {
+                parent::bootstrap($app);
+                Configure::write('TestPluginBoostrap', 'done');
+            }
+        };
+    }
+
+    /**
+     * Test `bootstrap` method.
+     *
+     * @return void
+     * @covers ::bootstrap()
+     */
+    public function testBootstrap(): void
+    {
+        $app = $this->createMock(PluginApplicationInterface::class);
+        $this->assertFalse(Configure::check('TestPluginBoostrap'));
+        $this->plugin->bootstrap($app);
+        $this->assertTrue(Configure::check('TestPluginBoostrap'));
+    }
+}
